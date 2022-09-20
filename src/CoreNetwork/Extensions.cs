c7d0a1e089aa459c118646a4ea2decece2c6928d
sp1x3r <96335468+isp1x3r@users.noreply.github.com>
2022-09-20 20:19:32 +0100

    Create Extensions.cs

---
+++ src/CoreNetwork/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreNetwork
+{
+    public static class Extensions
+    {
+        public static byte ReadAuthServerOpCode(byte[] packet)
+        {
+            byte opCode;
+            using (BinaryReader _r = new BinaryReader(new MemoryStream(packet)))
+            {
+                _r.BaseStream.Position = 3;
+                opCode = _r.ReadByte();
+            }
+            return opCode;
+        }
+        public static short ReadGameServerOpCode(byte[] packet)
+        {
+            short opCode;
+            using (BinaryReader _r = new BinaryReader(new MemoryStream(packet)))
+            {
+                _r.BaseStream.Position = 8;
+                opCode = _r.ReadInt16();
+            }
+            return opCode;
+        }
+    }
+}
