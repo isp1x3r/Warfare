f99e550211821f8119268f6ba0a71b3a5da3d2c3
sp1x3r <96335468+isp1x3r@users.noreply.github.com>
2022-09-25 14:23:09 +0100

    Quick update on gamemessages
    Forgot protobuf member isn't allowed to have id of 0 lul

--- src/AuthServer/Message/C2S.cs
+++ src/AuthServer/Message/C2S.cs
@@ -8,13 +8,13 @@ namespace AuthServer.Message
     [ProtoContract]
     public class CharacterCreateReqMessage
     {
-        [ProtoMember(0)]
+        [ProtoMember(1)]
         public string Nickname { get; set; }
 
-        [ProtoMember(1)]
+        [ProtoMember(2)]
         public CharacterHero Hero { get; set; }
 
-        [ProtoMember(2)]
+        [ProtoMember(3)]
         public byte SkinColor { get; set; }
     }
     [ClientMessage(1028)]
@@ -24,16 +24,16 @@ namespace AuthServer.Message
         /* Available only in JP version which is included in the client,
            so might as well add it here!
          */
-        [ProtoMember(0)]
+        [ProtoMember(1)]
         public uint Unk1 { get; set; }
 
-        [ProtoMember(1)]
+        [ProtoMember(2)]
         public uint Unk2 { get; set; }
 
-        [ProtoMember(2)]
+        [ProtoMember(3)]
         public CharacterHero Hero { get; set; }
 
-        [ProtoMember(3)]
+        [ProtoMember(4)]
         public byte Slot { get; set; }
 
     }
--- src/AuthServer/Message/S2C.cs
+++ src/AuthServer/Message/S2C.cs
@@ -1,4 +1,5 @@
-﻿using ProtoBuf;
+﻿using ServerCore;
+using ProtoBuf;
 
 
 namespace AuthServer.Message
@@ -7,43 +8,43 @@ namespace AuthServer.Message
     public class AuthenticationAckMessage
     {
 
-        [ProtoMember(0)]
+        [ProtoMember(1)]
         public ushort Errorcode { get; set; }
 
-        [ProtoMember(1)]
-        public uint Errorcode2 { get; set; } // lul
-
         [ProtoMember(2)]
-        public uint Unk1 { get; set; }
+        public uint Errorcode2 { get; set; }
 
         [ProtoMember(3)]
-        public long Unk2 { get; set; }
+        public uint Unk1 { get; set; }
 
         [ProtoMember(4)]
-        public short Unk3 { get; set; }
+        public long Unk2 { get; set; }
 
         [ProtoMember(5)]
-        public uint Unk4 { get; set; } // GameVersion??
+        public short Unk3 { get; set; }
 
         [ProtoMember(6)]
-        public byte Unk5 { get; set; }
+        public uint Unk4 { get; set; } // GameVersion??
 
         [ProtoMember(7)]
-        public uint Unk6 { get; set; }
+        public byte Unk5 { get; set; }
 
         [ProtoMember(8)]
-        public uint Unk7 { get; set; }
+        public uint Unk6 { get; set; }
 
         [ProtoMember(9)]
-        public uint Unk8 { get; set; } // Game Status or sth..
+        public uint Unk7 { get; set; }
 
         [ProtoMember(10)]
-        public byte Unk9 { get; set; }
+        public uint Unk8 { get; set; } // Game Status or sth..
 
         [ProtoMember(11)]
-        public byte Unk10 { get; set; }
+        public byte Unk9 { get; set; }
 
         [ProtoMember(12)]
+        public byte Unk10 { get; set; }
+
+        [ProtoMember(13)]
         public string ServerVersion { get; set; }
 
         public AuthenticationAckMessage()
@@ -56,16 +57,16 @@ namespace AuthServer.Message
     [ProtoContract]
     public class RetrieveCharacterAckMessage
     {
-        [ProtoMember(0)]
+        [ProtoMember(1)]
         public ushort Errorcode { get; set; }
 
-        [ProtoMember(1)]
+        [ProtoMember(2)]
         public uint CharacterCount { get; set; }
 
-        [ProtoMember(2)]
+        [ProtoMember(3)]
         public byte[] padding { get; set; }
 
-        [ProtoMember(3)]
+        [ProtoMember(4)]
         public string username { get; set; }
 
         public RetrieveCharacterAckMessage()
@@ -77,61 +78,61 @@ namespace AuthServer.Message
     [ProtoContract]
     public class CharacterInfoAckMessage
     {
-        [ProtoMember(0)]
+        [ProtoMember(1)]
         public ushort ErrorCode { get; set; }
 
-        [ProtoMember(1)]
+        [ProtoMember(2)]
         public uint Unk1 { get; set; }
 
-        [ProtoMember(2)]
+        [ProtoMember(3)]
         public string Nickname { get; set; }
 
-        [ProtoMember(3)]
+        [ProtoMember(4)]
         public ushort Level { get; set; }
 
-        [ProtoMember(4)]
+        [ProtoMember(5)]
         public CharacterHero Hero { get; set; }
 
-        [ProtoMember(5)]
+        [ProtoMember(6)]
         public uint Experience { get; set; }
 
-        [ProtoMember(6)]
+        [ProtoMember(7)]
         public uint BountyPoints { get; set; }
 
-        [ProtoMember(7)]
+        [ProtoMember(8)]
         public uint Unk2 { get; set; }
 
-        [ProtoMember(8)]
+        [ProtoMember(9)]
         public uint Kills { get; set; }
 
-        [ProtoMember(9)]
+        [ProtoMember(10)]
         public uint Deaths { get; set; }
 
-        [ProtoMember(10)]
+        [ProtoMember(11)]
         public uint Wins { get; set; }
 
-        [ProtoMember(11)]
+        [ProtoMember(12)]
         public int Losses { get; set; }
 
-        [ProtoMember(12)]
+        [ProtoMember(13)]
         public byte[] Padding { get; set; } // Has a length of 16 (suspecting a string)
 
-        [ProtoMember(13)]
+        [ProtoMember(14)]
         public uint Unk3 { get; set; }
 
-        [ProtoMember(14)]
+        [ProtoMember(15)]
         public uint Unk4 { get; set; }
 
-        [ProtoMember(15)]
+        [ProtoMember(16)]
         public string ClanName { get; set; }
 
-        [ProtoMember(16)]
+        [ProtoMember(17)]
         public string ClanMark { get; set; }
 
-        [ProtoMember(17)]
+        [ProtoMember(18)]
         public short SkinColor { get; set; }
 
-        [ProtoMember(18)]
+        [ProtoMember(19)]
         public byte ItemCount { get; set; }
 
         public CharacterInfoAckMessage()
@@ -144,10 +145,10 @@ namespace AuthServer.Message
     [ProtoContract]
     public class CharacterDeleteAckMessage
     {
-        [ProtoMember(0)]
+        [ProtoMember(1)]
         public CharacterScreenResult ScreenResult { get; set; }
 
-        [ProtoMember(1)]
+        [ProtoMember(2)]
         public uint Unk1 { get; set; }
 
         public CharacterDeleteAckMessage()
@@ -159,10 +160,10 @@ namespace AuthServer.Message
     [ProtoContract]
     public class ServiceConnectAckMessage
     {
-        [ProtoMember(0)]
+        [ProtoMember(1)]
         public CharacterScreenResult ScreenResult { get; set; }
 
-        [ProtoMember(1)]
+        [ProtoMember(2)]
         public byte[] Unk1 { get; set; }
 
         public ServiceConnectAckMessage()
@@ -174,37 +175,37 @@ namespace AuthServer.Message
     [ProtoContract]
     public class ServerListAckMessage
     {
-        [ProtoMember(0)]
+        [ProtoMember(1)]
         public ushort Unk1 { get; set; }
 
-        [ProtoMember(1)]
+        [ProtoMember(2)]
         public byte ServerEntries { get; set; }
 
-        [ProtoMember(2)]
+        [ProtoMember(3)]
         public byte Unk2 { get; set; }
 
-        [ProtoMember(3)]
+        [ProtoMember(4)]
         public byte Id { get; set; } // gotta check to make sure x')
 
-        [ProtoMember(4)]
+        [ProtoMember(5)]
         public ushort Unk3 { get; set; }
 
-        [ProtoMember(5)]
+        [ProtoMember(6)]
         public ushort Unk4 { get; set; }
 
-        [ProtoMember(6)]
+        [ProtoMember(7)]
         public short Unk5 { get; set; } // ServerPort Maybe?
 
-        [ProtoMember(7)]
+        [ProtoMember(8)]
         public uint Unk6 { get; set; } // ServerIP Maybe?
 
-        [ProtoMember(8)]
+        [ProtoMember(9)]
         public byte[] Unk7 { get; set; } // 241 bytes x')
 
-        [ProtoMember(9)]
+        [ProtoMember(10)]
         public byte[] Unk8 { get; set; } // Same 241 bytes again!
 
-        [ProtoMember(10)]
+        [ProtoMember(11)]
         public byte[] Unk9 { get; set; } // Only 33 bytes this time
 
         public ServerListAckMessage(byte id)
