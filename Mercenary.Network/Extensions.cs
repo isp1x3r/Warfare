573b7d0e3ae8fa2b700c8542b26e02bbb3aa4303
sp1x3r <96335468+isp1x3r@users.noreply.github.com>
2022-10-07 15:18:21 +0100

    Update Extensions.cs

--- Mercenary.Network/Extensions.cs
+++ Mercenary.Network/Extensions.cs
@@ -8,10 +8,11 @@ namespace Mercenary.Network
         public static string WriteCString(string @string, int length)
         {
             // Do we need to fill the remaining bytes?
-            if(length - @string.Length > 0)
+            int remaining = length - @string.Length;
+            if(remaining > 0)
             {
-                StringBuilder _builder = new StringBuilder(@string);
-                byte[] padding = new byte[length];
+                StringBuilder _builder = new StringBuilder(@string, length);
+                byte[] padding = new byte[remaining];
                 _builder.Append(Encoding.ASCII.GetString(padding));
                 return _builder.ToString();
             }
