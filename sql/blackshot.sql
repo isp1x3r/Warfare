3cad719f10a23eddd7bc02aa6a3fb48c8be2bba0
sp1x3r <96335468+isp1x3r@users.noreply.github.com>
2022-10-17 15:05:40 +0100

    Update blackshot.sql

--- sql/blackshot.sql
+++ sql/blackshot.sql
@@ -1,29 +1,97 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.6.13 - MySQL Community Server (GPL)
--- Server OS:                    Win32
--- HeidiSQL Version:             12.1.0.6537
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 17, 2022 at 02:05 PM
+-- Server version: 5.6.13
+-- PHP Version: 5.4.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Data exporting was unselected.
+--
+-- Database: `blackshot`
+--
+CREATE DATABASE IF NOT EXISTS `blackshot` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `blackshot`;
 
--- Data exporting was unselected.
+-- --------------------------------------------------------
 
--- Data exporting was unselected.
+--
+-- Table structure for table `accounts`
+--
 
--- Data exporting was unselected.
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `Id` int(10) unsigned NOT NULL,
+  `Username` varchar(17) NOT NULL DEFAULT '',
+  `Password` varchar(32) NOT NULL DEFAULT '',
+  `Country` varchar(2) DEFAULT '',
+  `LastIP` varchar(15) DEFAULT '',
+  `Account Number` int(10) unsigned NOT NULL,
+  `Cash` int(11) DEFAULT NULL,
+  `Slots` tinyint(1) DEFAULT NULL,
+  `PCEvent` tinyint(1) DEFAULT NULL COMMENT 'If player is in a LAN tournament(Garena''s implementation)',
+  KEY `Id` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bans`
+--
+
+CREATE TABLE IF NOT EXISTS `bans` (
+  `Account Number` int(10) DEFAULT NULL,
+  `Ban Reason` varchar(50) DEFAULT NULL,
+  `Ban Duration` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `characterinfo`
+--
+
+CREATE TABLE IF NOT EXISTS `characterinfo` (
+  `Account Number` int(10) DEFAULT NULL,
+  `Slot` tinyint(1) DEFAULT NULL,
+  `Nickname` varchar(17) DEFAULT NULL,
+  `Level` smallint(3) DEFAULT NULL,
+  `Hero` tinyint(4) DEFAULT NULL,
+  `Experience` int(11) DEFAULT NULL,
+  `Bounty Points` int(11) DEFAULT NULL,
+  `Kills` int(11) DEFAULT NULL,
+  `Deaths` int(11) DEFAULT NULL,
+  `Wins` int(11) DEFAULT NULL,
+  `Losses` int(11) DEFAULT NULL,
+  `Clan Name` int(11) DEFAULT NULL,
+  `Clan Mark` int(11) DEFAULT NULL,
+  `Skin Color` smallint(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `characteritems`
+--
+
+CREATE TABLE IF NOT EXISTS `characteritems` (
+  `Account Number` int(10) DEFAULT NULL,
+  `Slot` int(1) DEFAULT NULL,
+  `Item Code` int(11) DEFAULT NULL,
+  `Type` tinyint(4) DEFAULT NULL,
+  `Item Number` int(11) DEFAULT NULL,
+  `Minutes Left` int(11) DEFAULT NULL,
+  `Seconds Left` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
