1d12de6e565e3e592da32df16d686d0265e9ef47
sp1x3r <96335468+isp1x3r@users.noreply.github.com>
2022-10-21 20:57:49 +0100

    Update Extensions.cs

--- src/Warfare.Core/Extensions.cs
+++ src/Warfare.Core/Extensions.cs
@@ -59,19 +59,6 @@ namespace Warfare.Core
         {
             return Encoding.ASCII.GetString(w.ReadBytes(length));
         }
-        public static int GetManagedSize(Type type)
-        {
-            // all this just to invoke one opcode with no arguments!
-            var method = new DynamicMethod("GetManagedSizeImpl", typeof(uint), new Type[0], typeof(TypeExtensions), false);
-
-            ILGenerator gen = method.GetILGenerator();
-
-            gen.Emit(OpCodes.Sizeof, type);
-            gen.Emit(OpCodes.Ret);
-
-            var func = (Func<uint>)method.CreateDelegate(typeof(Func<uint>));
-            return checked((int)func());
-        }
     }
     
 }
