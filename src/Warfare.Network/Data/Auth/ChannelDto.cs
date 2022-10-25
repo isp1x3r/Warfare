c79867f2052a1c2e5af225ad538475e5667d8250
sp1x3r <96335468+isp1x3r@users.noreply.github.com>
2022-10-25 12:35:48 +0100

    Added Channel Message

---
+++ src/Warfare.Network/Data/Auth/ChannelDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BlubLib.Serialization;
+using System.Threading.Tasks;
+
+namespace Warfare.Network.Data.Auth
+{
+    [BlubContract]
+    public class ChannelDto
+    {
+        [BlubMember(1)]
+        public ushort PlayerCount { get; set; }
+
+        [BlubMember(2)]
+        public uint PlayerLimit { get; set; }
+
+        [BlubMember(3)]
+        public ushort Pad { get; set; }
+
+        [BlubMember(4)]
+        public byte Unk1 { get; set; }
+
+        [BlubMember(5)]
+        public byte Unk2 { get; set; }
+
+        public ChannelDto()
+        {
+
+        }
+    }
+}
--- src/Warfare.Server.Auth/Handlers/ChannelListHandler.cs
+++ src/Warfare.Server.Auth/Handlers/ChannelListHandler.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Warfare.Core;
+using Warfare.Network.Data.Auth;
 using Warfare.Network.Message.Auth;
 
 namespace Warfare.Server.Auth.Handlers
@@ -17,6 +18,19 @@ namespace Warfare.Server.Auth.Handlers
         }
         public bool Handle(Session session, ChannelListReqMessage message)
         {
+            session.SendAsync(new ChannelListAckMessage()
+            {
+                Channels = new ChannelDto[]
+                {
+                    new ChannelDto
+                    {
+                        Unk1 = 1,
+                        Unk2 = 3,
+                        PlayerCount = 70,
+                        PlayerLimit = 450
+                    }
+                }
+            });
             return true;
         }
     }
